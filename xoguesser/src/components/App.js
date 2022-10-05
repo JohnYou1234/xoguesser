6ae7fb5c7cd2ed30074b7fa7d9d0be2d9a154ff4
John <yyoungjune@gmail.com>
2022-10-04 18:59:13 -0700

    little bit of centering

--- xoguesser/src/components/App.js
+++ xoguesser/src/components/App.js
@@ -1,15 +1,34 @@
 import Display from './Display.js';
 import Controls from './Controls.js'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Guess The Input
-        </p>
       </header>
-      <Display />
-      <Controls />
+      <Container className='centered'>
+        <Row>
+          <Col>
+            <p>
+            Guess The Input
+            </p>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col>
+            <Display />
+          </Col>
+        </Row>
+
+        <Row>
+          <Col>
+            <Controls />
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
--- xoguesser/src/components/Controls.js
+++ xoguesser/src/components/Controls.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import Button from 'react-bootstrap/Button';
 
 function Controls() {
-
+    
     return (
         <div className='controls'>
             <GuessBtn name="Manual"></GuessBtn>
